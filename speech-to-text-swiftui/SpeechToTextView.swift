5faa5d72b83e7f7c39e7439625b3c0fb04941c1d
aembadawy <aem.badawy@gmail.com>
2025-05-17 12:11:18 +0300

    Add speech recognizer UIView

--- speech-to-text-swiftui/SpeechToTextView.swift
+++ speech-to-text-swiftui/SpeechToTextView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct SpeechToTextView: View {
+    @State private var isRecording = false
+    @State private var viewModel = SpeechRecognitionViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            Image(systemName: "waveform")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundStyle(Color.blue)
+            
+            Text("Speechify")
+                .font(.headline)
+            
+            ScrollView {
+                Text(
+                    viewModel.transcript.isEmpty ? "Transcripted speech will apear here..." : viewModel.transcript)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .background(Color.gray)
+                .cornerRadius(12)
+            }
+            
+            Button(isRecording ? "Stop Recording" : "Start Recording") {
+                isRecording.toggle()
+                
+                if isRecording {
+                    viewModel.startRecognition()
+                } else {
+                    viewModel.stopRecognition()
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 48)
+            .background(isRecording ? .red : .blue)
+            .foregroundStyle(.white)
+            .font(.headline)
+            .cornerRadius(12)
+            
+        }
+        .padding()
+        
     }
 }
 
