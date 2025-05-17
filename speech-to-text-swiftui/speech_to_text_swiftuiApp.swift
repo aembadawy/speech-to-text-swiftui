47c42fe58d056049beb80fc0fb386dd683195c94
aembadawy <aem.badawy@gmail.com>
2025-05-17 07:33:19 +0300

    Initial Commit

---
+++ speech-to-text-swiftui/speech_to_text_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  speech_to_text_swiftuiApp.swift
+//  speech-to-text-swiftui
+//
+//  Created by Aya on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct speech_to_text_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ speech-to-text-swiftuiTests/speech_to_text_swiftuiTests.swift
@@ -0,0 +1,17 @@
+//
+//  speech_to_text_swiftuiTests.swift
+//  speech-to-text-swiftuiTests
+//
+//  Created by Aya on 17/05/2025.
+//
+
+import Testing
+@testable import speech_to_text_swiftui
+
+struct speech_to_text_swiftuiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
